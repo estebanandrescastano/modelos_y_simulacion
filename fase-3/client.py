54672925fb691454824250a4de949d05471a6614
estebanandrescastano <130505968+estebanandrescastano@users.noreply.github.com>
2026-05-04 20:14:36 -0500

    Update client.py

--- fase-3/client.py
+++ fase-3/client.py
@@ -1,1 +1,28 @@
+import requests
+import pandas as pd
+
+url = 'http://localhost:5000/predict'
+data = {
+    "Age": [63],
+    "Sex": [1],
+    "Chest pain type": [3],
+    "BP": [145],
+    "Cholesterol": [233],
+    "FBS over 120": [1],
+    "EKG results": [0],
+    "Max HR": [150],
+    "Exercise angina": [0],
+    "ST depression": [2.3],
+    "Slope of ST": [0],
+    "Number of vessels fluro": [0],
+    "Thallium": [1]
+}
+
+response = requests.post(url, json=data)
+
+if response.status_code == 200:
+    print(f"Respuesta exitosa: {response.json()}")
+else:
+    print(f"Error del servidor (Código {response.status_code}):")
+    print(response.text)  
 
